f16bf70bfc48f75006d90ce486fc31832ca7dfe6
kumar1397 <sahuiswar007@outlook.com>
2024-06-24 20:37:41 +0530

    1

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 async function fetchBlogs() {
   const res = await fetch("http://localhost:3000/api/blog", {
     next: {
-      revalidate:5,
+      revalidate: 10,
     },
   });
   const data = await res.json();
@@ -13,7 +13,6 @@ async function fetchBlogs() {
 export default async function Home() {
   const posts = await fetchBlogs();
   console.log(posts);
-
   return (
     <main className="w-full h-full">
       <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-slate-800 drop-shadow-xl">
